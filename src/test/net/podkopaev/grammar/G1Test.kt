06322c11923e8eff8cb03e6888f68a50d180ff0a
onewhl <onewhl8@gmail.com>
2016-11-12 00:19:57 +0300

    Added test for Example1

--- src/test/net/podkopaev/grammar/G1Test.kt
+++ src/test/net/podkopaev/grammar/G1Test.kt
@@ -1,21 +1,19 @@
-package net.podkopaev
+package net.podkopaev.grammar
 
-import net.podkopaev.grammar.*
-import net.podkopaev.booleanComb.*
+import net.podkopaev.grammar.Example1.*
 
 import org.junit.Assert
 import org.junit.Test
 
-class GTest {
+class G1Test {
     @Test fun test0() {
         val p = grParser
-        //val result = p.get("aaakbbbkccc")
-        //Assert.assertEquals("c", result.toString())
+        val result = p.get("aaakbbbkccc")
+        Assert.assertEquals("c", result.toString())
     }
-
     @Test fun test1() {
-        val m = pC
-        val result = m.get("aacaa")
-        Assert.assertEquals("a", result.toString())
+        val p = pD
+        val result = p.get("aaakbbb")
+        Assert.assertEquals("b", result.toString())
     }
 }
