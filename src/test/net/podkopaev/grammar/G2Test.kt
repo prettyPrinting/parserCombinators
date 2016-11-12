a17a04e128ba5d6f1a5c40dc12956905bdec38e1
onewhl <onewhl8@gmail.com>
2016-11-12 16:44:48 +0300

    Added part of test for Example 2

--- src/test/net/podkopaev/grammar/G2Test.kt
+++ src/test/net/podkopaev/grammar/G2Test.kt
@@ -6,9 +6,28 @@ import org.junit.Assert
 import org.junit.Test
 
 class G2Test {
+    // {lcl | l = (a | b)*}
     @Test fun test0() {
-        val p = pA
-        val result = p.get("ackaa")
-        Assert.assertEquals("a", result.toString())
+        val p = grParser
+        val result = p.get("abaabbacabaabba")
+        Assert.assertEquals("(a, a)", result.toString())
+    }
+    // {b^n c b^n | n = 5}
+    @Test fun test1() {
+        val p = grParser
+        val result = p.get("bbbbbcbbbbb")
+        Assert.assertEquals("(b, b)", result.toString())
+    }
+    // {a^n c a^n | n = 10}
+    @Test fun test2() {
+        val p = grParser
+        val result = p.get("aaaaaaaaaacaaaaaaaaaa")
+        Assert.assertEquals("(a, a)", result.toString())
+    }
+    // {lcw | l != w}
+    @Test fun test3() {
+        val p = grParser
+        val result = p.get("abacbab")
+        Assert.assertNotEquals("(b, b)", result.toString())
     }
 }
