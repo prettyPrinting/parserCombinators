f53c884a7144de3129b4694a1c2ccc38d03be24b
onewhl <onewhl8@gmail.com>
2017-02-21 23:32:35 +0300

    Implemented base cps parsers.

--- src/main/net/podkopaev/cpsComb.kt
+++ src/main/net/podkopaev/cpsComb.kt
@@ -2,104 +2,84 @@ package net.podkopaev.cpsComb
 
 import java.util.*
 
-abstract class MatchFail<A>() {
-    abstract fun match(startPos: Int, endPos: Int, result: A)
-    abstract fun fail (startPos: Int)
+abstract class Result<A>() { }
+class Success<A>(val value: A, val rest: Int) : Result<A>() { }
 
-    abstract fun init(s: String)
-}
+class Failure<A>(rest: Int) : Result<A>() { }
 
 abstract class Parser<A>() {
-    public val klist: ArrayList<MatchFail<A>> = ArrayList()
-    public var parseString: String? = null
+    var parseString: String? = null
 
     internal open fun init(s: String) {
         parseString = s
     }
 
-    abstract operator fun invoke(pos: Int)
+    abstract operator fun invoke(pos: Int, c: (Result<A>) -> Unit): Unit
 }
 
-class MatchFailMap<A>(): MatchFail<A>() {
-    public var resultMap : HashMap<Int, Pair<Int, A>> = HashMap()
-
-    override fun match(startPos : Int, endPos : Int, result : A) {
-        if (startPos in resultMap) { return }
-        resultMap.put(startPos, Pair(endPos, result))
-    }
-    override fun fail (startPos : Int) {}
-
-    override fun init(s: String) {
-        resultMap = HashMap()
-    }
-}
-
-fun <A> parser(invokeBody : Parser<A>.(Int) -> Unit): Parser<A> =
+fun <A> conp(value: A): Parser<A> =
         object : Parser<A>() {
-            override fun invoke(pos: Int) = invokeBody(pos)
+            override fun invoke(pos: Int, c: (Result<A>) -> Unit) {
+                c(Success(value, pos))
+            }
         }
 
-fun <A> conp(value: A): Parser<A> = parser { pos ->
-    for (k in klist) {
-        k.match(pos, pos, value)
-    }
-}
-
-fun litp(str: String): Parser<String> = parser { pos ->
-    val substring = parseString!!.substring(pos)
-    if (!substring.startsWith(str)) {
-        for (k in klist) {
-            k.fail(pos)
+fun litp(str: String): Parser<String> =
+        object : Parser<String>() {
+            override fun invoke(pos: Int, c: (Result<String>) -> Unit) {
+                val substring = parseString!!.substring(pos)
+                if (!substring.startsWith(str)) {
+                    c(Failure(pos))
+                }
+                c(Success(str, pos))
+            }
         }
-        return@parser
-    }
-    for (k in klist) {
-        k.match(pos, pos + str.length, str)
-    }
-}
 
-fun satp(cond: (Char) -> Boolean): Parser<Char> = parser { pos ->
-    val substring = parseString!!.substring(pos)
-    if (substring.length < 1 || !cond(substring[0])) {
-        return@parser
-    }
-    for (k in klist) {
-        k.match(pos, pos + substring.length, substring[0])
-    }
-}
+fun satp(cond: (Char) -> Boolean): Parser<Char> =
+        object : Parser<Char>() {
+            override fun invoke(pos: Int, c: (Result<Char>) -> Unit) {
+                val substring = parseString!!.substring(pos)
+                if (substring.length < 1 || !cond(substring[0])) {
+                    c(Failure(pos))
+                }
+                c(Success(substring[0], 1))
+            }
+        }
 
-fun char(c: Char): Parser<Char> = satp { it == c }
+fun <A, B> bindp(left: Parser<A>, right: (A) -> Parser<B>): Parser<B> =
+        object : Parser<B>() {
+            override fun invoke(pos: Int, c: (Result<B>) -> Unit) {
+                left(pos) { r ->
+                    when (r) {
+                        is Success<A> -> {
+                            right(r.value)(r.rest, c)
+                        }
+                        is Failure<A> -> {
+                            c(Failure(pos))
+                        }
+                    }
+                }
+            }
+        }
+fun <A> seqp(left: Parser<A>, right: Parser<A>) =
+        object : Parser<A>() {
+            override fun invoke(pos: Int, c: (Result<A>) -> Unit) {
+                bindp(left) { right }
+            }
+        }
+
+fun <A, B> transp(parser: Parser<A>, f: (A) -> B): Parser<B> =
+        bindp(parser) { conp(f(it)) }
+
+fun <A> disjp(left: Parser<A>, right: Parser<A>): Parser<A> =
+        object : Parser<A>() {
+            override fun invoke(pos: Int, c: (Result<A>) -> Unit) {
+                left(pos, c)
+                right(pos, c)
+            }
+        }
+
 val digit: Parser<Char> = satp { ('0'..'9').contains(it) }
 val alpha: Parser<Char> = satp {
     ('a'..'z').contains(it) || ('A'..'Z').contains(it)
 }
-/*
-fun <A, B> transp(parser: Parser<A>, f: (A) -> B): Parser<B> = parser { pos ->
-    parser.klist.add(
-            object: MatchFail<A>() {
-                override fun match(startPos: Int, endPos: Int, result: A) {
-                    this.match(startPos, endPos, f(result))
-                }
-
-                override fun fail(startPos: Int) {
-                    this.fail(startPos)
-                }
-
-                override fun init(s: String) {
-                    this.init(s)
-                }
-            }
-    )
-    parser(pos)
-}
-*/
-
-
-
-
-
-
-
-
-
-
