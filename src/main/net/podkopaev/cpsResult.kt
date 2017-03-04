50eb5397c331f6620c91b4e700a04c9a127251a2
onewhl <onewhl8@gmail.com>
2017-03-05 00:50:03 +0300

    Started rewriting parsers.

---
+++ src/main/net/podkopaev/cpsResult.kt
@@ -0,0 +1,62 @@
+package net.podkopaev.cpsResult
+
+import java.util.*
+
+abstract class Result <A> {
+    abstract operator fun  invoke(k: (A) -> Unit)
+}
+
+abstract class CPSResult<A> {
+    fun <A> result(f: ((A) -> Unit) -> Unit) =
+            object : Result<A>() {
+                override fun invoke(k: (A) -> Unit) = f(k)
+            }
+
+    fun <A> success(t: A): Result<A> = result { k -> k(t) }
+    fun <A> failure(): Result<A> = result { k -> { } }
+
+    fun <A> memo_k(f: (A) -> Unit): (A) -> Unit {
+        val s: HashSet<A> = HashSet()
+        return { t -> if(!s.contains(t)) { s += t; f(t) } }
+    }
+
+    fun <B> map(f: (A) -> B): Result<B> = result { k ->
+        this(memo_k{ t -> k(f(t)) }) }
+    fun <B> flatMap(f: (A) -> Result<B>): Result<B> =
+            result{ k -> this(memo_k { t -> f(t)(k) })}
+    fun <A> orElse(r: () -> Result<A>) = { }
+
+    abstract operator fun  invoke(k: (A) -> Unit)
+}
+
+abstract class MemoizedCPS<A>: MemoizedCPSResult<A>() {
+    fun <A> memo(f: (Int) -> Result<A>): (Int) -> Result<A>? {
+        val table: MutableMap<Int, Result<A>> = HashMap()
+        return { i: Int -> table.getOrElse(i) { val v = memo_result({f(i)}); table.put(i, v) } }
+    }
+}
+
+abstract class MemoizedCPSResult<A> : CPSResult<A>() {
+    fun <A> memo_result(res: () -> Result<A>): Result<A> {
+        val Rs: MutableList<A> = ArrayList()
+        val Ks: MutableList<(A) -> Unit> = ArrayList()
+        return result { k ->
+            if (Ks.isEmpty()) {
+                Ks += k
+                val ki: (A) -> Unit = { t ->
+                    if (!Rs.contains(t)) {
+                        Rs += t; for (kt in Ks) kt(t)
+                    }
+                }
+                //res(ki)
+            } else {
+                Ks += k
+                for (t in Rs) k(t)
+            }
+        }
+    }
+}
+
+abstract class Recognizers {
+    val input: String? = null
+}
