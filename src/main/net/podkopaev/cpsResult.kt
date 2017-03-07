16901f72d409b60b01846bb497e451c8317fe89e
onewhl <onewhl8@gmail.com>
2017-03-08 02:19:45 +0300

    Added typealias for continuation type.

--- src/main/net/podkopaev/cpsResult.kt
+++ src/main/net/podkopaev/cpsResult.kt
@@ -2,33 +2,37 @@ package net.podkopaev.cpsResult
 
 import java.util.*
 
-abstract class Result <A> {
-    abstract operator fun  invoke(k: (A) -> Unit)
+typealias K<A> = (A) -> Unit
+
+abstract class Result<A> {
+    abstract operator fun  invoke(k: K<A>)
 }
 
 abstract class CPSResult<A> {
-    fun <A> result(f: ((A) -> Unit) -> Unit) =
+    fun <A> result(f: (K<A>) -> Unit) =
             object : Result<A>() {
-                override fun invoke(k: (A) -> Unit) = f(k)
+                override fun invoke(k: K<A>) = f(k)
             }
 
     fun <A> success(t: A): Result<A> = result { k -> k(t) }
     fun <A> failure(): Result<A> = result { k -> { } }
 
-    fun <A> memo_k(f: (A) -> Unit): (A) -> Unit {
+    fun <A> memo_k(f: K<A>): K<A> {
         val s: HashSet<A> = HashSet()
         return { t -> if(!s.contains(t)) { s += t; f(t) } }
     }
 
-    fun <B> map(f: (A) -> B): Result<B> = result { k ->
-        this(memo_k{ t -> k(f(t)) }) }
+    fun <B> (Result<Int>).map(f: (Int) -> B): Result<B> = result { k ->
+        this(memo_k { t -> k(f(t)) }) }
+
     fun <B> (Result<Int>).flatMap(f: (Int) -> Result<B>): Result<B> =
             result{ k -> this(memo_k { t -> f(t)(k) })}
-    fun <A> (Result<Int>).orElse(r: ((Int) -> Unit) -> Result<A>) = {
-        lazy { val v = r; result { k: (Int) -> Unit -> this(k); v(k) } }
+
+    fun <A> (Result<Int>).orElse(r: (K<Int>) -> Result<A>) = {
+        lazy { val v = r; result { k: K<Int> -> this(k); v(k) } }
     }
 
-    abstract operator fun  invoke(k: (A) -> Unit)
+    abstract operator fun  invoke(k: K<A>)
 }
 
 abstract class MemoizedCPS<A>: MemoizedCPSResult<A>() {
@@ -39,7 +43,7 @@ abstract class MemoizedCPS<A>: MemoizedCPSResult<A>() {
 }
 
 abstract class MemoizedCPSResult<A> : CPSResult<A>() {
-    fun <A> memo_result(res: ((A) -> Unit) -> Result<A>): Result<A> {
+    fun <A> memo_result(res: (K<A>) -> Result<A>): Result<A> {
         val Rs: MutableList<A> = ArrayList()
         val Ks: MutableList<(A) -> Unit> = ArrayList()
         return result { k ->
@@ -70,7 +74,7 @@ abstract class Recognizers<A>() : CPSResult<A>() {
     fun epsilon(): (Int) -> Result<Int> = { i -> success(i) }
 
     fun seq(r1: (Int) -> Result<Int>, r2: (Int) -> Result<Int>): (Int) -> Result<Int> = {
-        i -> r1(i).flatMap({ r2(i) })
+        i -> r1(i).flatMap{ r2(i) }
     }
 
     fun alt(r1: (Int) -> Result<Int>, r2: (Int) -> Result<Int>) = {
